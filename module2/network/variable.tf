99e988ca287c7359a47672e70d0e3db7fd774fa6
RoshanFathima <shanufahima9207@gmail.com>
2022-04-15 18:49:23 +0000

    Modules for Prod Networking

---
+++ module2/network/variable.tf
@@ -0,0 +1,66 @@
+# Default tags
+variable "default_tags" {
+  default     = {}
+  type        = map(any)
+  description = "Default tags to be appliad to all AWS resources"
+}
+
+# Name prefix
+variable "prefix" {
+  type        = string
+  description = "Name prefix"
+}
+
+# Provision public subnets in custom VPC
+#variable "public_cidr_blocks" {
+ # type        = list(string)
+  #description = "Public Subnet CIDRs"
+#}
+
+# Provision private subnets in custom VPC
+#variable "private_cidr_blocks" {
+ # type        = list(string)
+  #description = "Private Subnet CIDRs"
+#}
+
+# VPC CIDR range
+#variable "vpc_cidr" {
+ # type        = string
+  #description = "Prod VPC"
+#}
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "prod"
+  #{["env1" = "staging"
+  #"env2" = "prod"]}
+  type        = string
+  description = "Environment"
+}
+
+variable "awsbc" {
+  default     = ["us-east-1b","us-east-1c","us-east-1d"]
+  type        = list(string)
+  description = "AWS regions"
+}
+
+# Provision public subnets in custom VPC
+variable "public_subnet_cidrs" {
+  default     = ["10.250.1.0/24", "10.250.2.0/24", "10.250.3.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs"
+}
+
+# Provision private subnets in custom VPC
+variable "private_subnet_cidrs" {
+  default     = ["10.250.4.0/24", "10.250.5.0/24", "10.250.6.0/24"]
+  type        = list(string)
+  description = "Private Subnet CIDRs"
+}
+
+# VPC CIDR range
+variable "vpc_cidr" {
+  default     = "10.250.0.0/16"
+  type        = string
+  description = "prod VPC"
+}
