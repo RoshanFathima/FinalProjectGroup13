69982080f50d85487c8c1c5abd36903a0ff80353
lakshayanand14 <lakshayanand97@gmail.com>
2022-04-15 22:36:42 +0000

    Prod Environment deployment for Networking and Webservers

---
+++ prod/vm/variable.tf
@@ -0,0 +1,33 @@
+# Instance type
+variable "instance_type" {
+  default     = "t3.small"
+  description = "Type of the instance"
+  type        = string
+}
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "prod"
+  type        = string
+  description = "ProdEnvironment"
+}
+
+variable "my_private_ip" {
+  type        = string
+  default     = "172.31.42.150"
+  description = "Private IP of my Cloud 9 station to be opened in bastion ingress"
+}
+
+# curl http://169.254.169.254/latest/meta-data/public-ipv4
+variable "my_public_ip" {
+  type        = string
+  default     = "34.228.233.151"
+  description = "Public IP of my Cloud 9 station to be opened in bastion ingress"
+}
+
+variable "service_ports" {
+  type        = list(string)
+  default     = ["80", "22"]
+  description = "Ports that should be open on a webserver"
+}
+
