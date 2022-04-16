b4c0b20558bdc370e2eaf4bde77fa919e16ec444
rkrnarala <103586120+rkrnarala@users.noreply.github.com>
2022-04-16 05:05:35 -0400

    Update main.tf

--- staging/stagingup/vm/main.tf
+++ staging/stagingup/vm/main.tf
@@ -201,7 +201,7 @@ resource "aws_instance" "bastion" {
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.key2b.key_name
   subnet_id                   = data.terraform_remote_state.network.outputs.public_subnet_ids[1]
-  security_groups             = [aws_security_group.albsg.id]
+  security_groups             = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
 
 
