8ad515d2ad8e65648eebef5eea86aa3fae345ed6
lakshayanand14 <70287043+lakshayanand14@users.noreply.github.com>
2022-04-16 03:42:42 -0400

    Update config.tf

--- prod/vm/config.tf
+++ prod/vm/config.tf
@@ -4,10 +4,4 @@ terraform {
     key    = "prod-vm/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
     region = "us-east-1"                 // Region where bucket is created
   }
-}terraform {
-  backend "s3" {
-    bucket = "prod-group13"              // Bucket where to SAVE Terraform State
-    key    = "prod-vm/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                 // Region where bucket is created
-  }
 }
